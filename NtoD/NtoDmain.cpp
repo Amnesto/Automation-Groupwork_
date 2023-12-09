ef22b8bac08ea842eb729a7f5cdfa686dd4aee42
Amnesto <128667024+Amnesto@users.noreply.github.com>
2023-12-09 22:04:07 +0800

    Add files via upload

--- NtoD/NtoDmain.cpp
+++ NtoD/NtoDmain.cpp
@@ -1,4 +1,7 @@
-﻿//子集s
+﻿//Epset:存储结点的容器（未定）
+set s;
+//子集s
+queue q;
 //队列q
 
 //算起始状态空闭包给I0
@@ -23,4 +26,25 @@ while (!q.empty()) {
 			}
 		}
 	}
+}
+
+
+Epset epcloure(Epset s) {
+	stack<int> epStack;
+
+	Epset::iterator it;
+	for (it = s.begin(); it != s.end(); it++) {
+		epStack.push(it);
+	}
+
+	while (!epStack.empty()) {
+		int temp = epStack.top();
+		epStack.pop();
+
+		//遍历通过空能到状态集
+		//若当前未出现则加入集合并压入栈中
+		
+	}
+
+	return s;
 }
