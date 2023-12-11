87dfe8781062c6f0bbe953ef6be11b3a3226a6d5
Amnesto <128667024+Amnesto@users.noreply.github.com>
2023-12-12 02:40:09 +0800

    Add files via upload

--- NtoD/NtoDmain.cpp
+++ NtoD/NtoDmain.cpp
@@ -1,7 +1,14 @@
-﻿Epset epcloure(Epset s);
+﻿#include "macros.hpp"
+#include "Node.hpp"
+
+
+
+Epset epcloure(Epset s);
 //求状态集的空闭包状态集
 Epset epcloureMove(Epset s, char ch);
 //求状态集在经过ch后到的空闭包状态集
+bool isEnd();
+//判断DFA状态是否是终态
 
 
 
@@ -9,18 +16,17 @@ Unknown NfaToDfa() {
 	//Epset:存储结点的容器（未定）
 	Epset tempSet;
 
-	set s;
+	set<Epset> s;
 	//子集s
-	queue q;
+	queue<int> q;
 	//队列q
 
-	tempSet.insert(NFA初态)；
+	tempSet.insert(NFA初态编号)；
 
-	DFA初态 = epcloure(Epset);
-	//算起始状态空闭包给I0
-	DFA状态数量自加；
+	DFA初态.对应NFA空闭包集合 = epcloure(Epset);
+	DFA初态.End = isEnd();
+	DFA状态数量++；
 	q.push(0);
-	//空闭包进入s和q
 
 
 	while (!q.empty()) {
@@ -29,24 +35,30 @@ Unknown NfaToDfa() {
 		q.pop();
 
 		for （非终结符）{
-			Epset temp = epcloureMove(n对应DFA状态集， * it);
-			//temp_s = I读ch后的空闭包
+			Epset temp = epcloureMove(DFA状态n.对应NFA空闭包集合， * it);
 
 			if (!temp.empty()) {
 				if (!s.count(temp)) {
 					s.insert(temp);
+
+					DFA状态n.对应NFA闭包集合 = temp;
+
+					处理边（将终结符放到边上，转移到最后一个DFA状态（DFA状态数））;
+					DFA初态.End = isEnd();
+
 					q.push(DFA状态数);
 
 					判断是否是终态;
-					DFA状态数自加;
+					DFA状态数++;
 
 				}
 
 				else {
 
 					for (int i = 0; i < DFA状态数; i++) {
-						if (temp == i对应的DFA状态) {
-							//为当前I新建一条Edege；
+						if (temp == DFA状态i.对应NFA空闭包集合) {
+
+							处理边（将终结符放到边上，转移到第i个状态）;
 							break;
 						}
 					}
@@ -74,8 +86,13 @@ Epset epcloure(Epset s) {
 		int temp = epStack.top();
 		epStack.pop();
 
-		//遍历temp通过空能到状态集
-		//若当前未出现则加入集合并压入栈中
+		Epset::iterator iter;
+		for (iter = NFA状态temp.对应空闭包.begin(); iter != NFA状态temp.对应空闭包.end(); iter++) {
+			if (!s.count(*iter)) {
+				s.insert(*iter);
+				epStack.push(*iter);
+			}
+		}
 		
 	}
 
@@ -87,10 +104,14 @@ Epset epcloureMove(Epset s, char ch) {
 
 	Epset::iterator it;
 	for (it = s.begin(); it != s.end(); it++) {
-		if (对应边为ch) {
-			temp.insert(通过边到的状态);
+		if (对应NFA转换边为ch) {
+			temp.insert(通过转换边到的状态);
 		}
 	}
 	temp = epcloure(temp);
 	return temp;
+}
+
+bool isEnd() {
+
 }
